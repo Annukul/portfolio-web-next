671ef7e4e323c3e936142ebea318d4d557bba1b4
Annukul Thakran <annukulthakran@Annukuls-MacBook-Air.local>
2023-04-08 23:14:00 +0530

    New UI added

---
+++ src/components/sections/about-me/about-me.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+
+export const AboutMe = () => {
+  return (
+    <div className="w-full h-screen bg-white py-8">
+      <div className="flex w-full h-full px-32">
+        <div className="w-2/4">
+          <p style={{ color: "#00FFEF" }} className="text-2xl font-bold">
+            About Me.
+          </p>
+          <div className="border mt-2"></div>
+          <div className="text-lg font-normal">
+            <p className="mt-2">
+              Hello! My name is Annukul and I enjoy creating things that create
+              an impact on life of people. My interest in programming started
+              back in initial days of my college when I decided to start my own
+              blog on wordpress. And later I got to know about web development
+              and how you can develop your own website using HTML, CSS, and JS.
+            </p>
+            <p className="mt-4">
+              I started my web development learning journey on FreeCodeCamp. And
+              gradually I started loving what I was doing and then created
+              several projects on my own.
+            </p>
+            <p className="mt-4">
+              Fast-forwarding to today, I have worked with multiple clients in
+              building their wep app from scratch and also re-writing old web
+              apps. I have also worked on several native consumer facing mobile
+              applications using React Native.
+            </p>
+            <p className="mt-4">
+              Currently I am working in{" "}
+              <span style={{ color: "#00FFEF" }}>Produze</span> as a{" "}
+              <span style={{ color: "#00FFEF" }}>
+                Full-stack Software Engineer
+              </span>{" "}
+              where I am building the consumer facing metrics dashboard which
+              involves handling tons of metrics data.
+            </p>
+            <p className="mt-4">
+              Here are a few technologies I’ve been working with recently:
+            </p>
+            <div className="flex py-4">
+              <ul className="mr-12">
+                <li>
+                  <span className="font-bold">1. </span>JavaScript (ES6+)
+                </li>
+                <li>
+                  <span className="font-bold">2. </span>React.js
+                </li>
+                <li>
+                  <span className="font-bold">3. </span>TypeScript
+                </li>
+                <li>
+                  <span className="font-bold">4. </span>Next.js
+                </li>
+                <li>
+                  <span className="font-bold">5. </span>Node.js
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <span className="font-bold">6. </span>Elixir
+                </li>
+                <li>
+                  <span className="font-bold">7. </span>Pheonix Framework
+                </li>
+                <li>
+                  <span className="font-bold">8. </span>Postgres
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-center w-2/4">
+          <div
+            style={{ borderColor: "#00FFEF" }}
+            className="flex items-center justify-center border-4 w-80 p-4 rotate-12"
+          >
+            <Image
+              className="-rotate-12"
+              src="/images/me.jpg"
+              alt="Author image"
+              width={300}
+              height={500}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/sections/experience/experience.tsx
@@ -0,0 +1,126 @@
+export const Experience = () => {
+  return (
+    <div
+      style={{ backgroundColor: "#0A1930" }}
+      className="w-full h-screen py-8"
+    >
+      <div className="flex w-full h-full px-32">
+        <div className="w-3/4 ml-32 text-white">
+          <p style={{ color: "#00FFEF" }} className="text-2xl font-bold">
+            Experience.
+          </p>
+          <div className="text-lg">
+            <div className="py-2">
+              <p className="font-normal">
+                <span className="font-bold">1. </span>Solidity Technologies (Dec
+                - 2021 to Jan 2023)
+              </p>
+              <div>
+                <div className="border mt-2"></div>
+                <ul className="list-disc mt-4">
+                  <li>
+                    <p>
+                      Re-wrote the entire B2C platform for the unicorn startup
+                      of India.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Improved the architecture of the platform using Duck
+                      architecture.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Consistently meet deadlines and requirements for all
+                      production work orders.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Collaborated with in-house web designers to create a sleek
+                      and innovative UI design.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Provided ongoing maintenance and development of bug fixes
+                      and patch sets for existing web applications for car
+                      reselling and logistics platforms
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Tested websites and performed troubleshooting prior to
+                      deployment.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Developed native mobile application from scratch using
+                      React native.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Fixed bugs and improved UI of Native consumer facing
+                      application.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div
+                style={{ borderColor: "#00FFEF" }}
+                className="border px-4 py-2 mt-2"
+              >
+                <p>
+                  <span className="text-bold">Skills:</span> Android Studio ·
+                  Redux · Git · Tailwind CSS · React Native · JavaScript ·
+                  React.js · TypeScript · Node.js · Nest.js
+                </p>
+              </div>
+            </div>
+            <div className="py-4 mt-4">
+              <p className="font-normal">
+                <span className="font-bold">2. </span>Produze (Jan 2023 to
+                Present)
+              </p>
+              <div>
+                <div className="border mt-2"></div>
+                <ul className="list-disc mt-4">
+                  <li className="py-1">
+                    <p>
+                      Re-writing the entire metrics dashboard for consumer
+                      facing web platform.
+                    </p>
+                  </li>
+                  <li className="py-1">
+                    <p>
+                      Integrating Amazon APIs to continously fetch sales data.
+                    </p>
+                  </li>
+                  <li className="py-1">
+                    <p>
+                      Working with elixir and phoenix to build a scalable
+                      backend application.
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div
+                style={{ borderColor: "#00FFEF" }}
+                className="border px-4 py-2 mt-2"
+              >
+                <p>
+                  <span className="text-bold">Skills:</span> Git · Tailwind CSS
+                  · JavaScript · React.js · Next.js · TypeScript · Elixir ·
+                  Pheonix · PostgreSQL
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
