02688f731b96048c30bfa3ac07a1ff1c2383c0aa
Annukul Thakran <annukulthakran@Annukuls-MacBook-Air.local>
2023-04-09 11:35:41 +0530

    Responsive for mobile

--- src/components/sections/main-section/main-section.tsx
+++ src/components/sections/main-section/main-section.tsx
@@ -1,25 +1,45 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import { BsTwitter, BsGithub, BsMailbox2, BsLinkedin } from "react-icons/bs";
 import { SiHashnode } from "react-icons/si";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 export const MainSection = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
-    <div className="h-screen flex flex-col justify-between items-center w-full">
-      <nav className={styles.nav}>
+    <div className="h-screen flex flex-col justify-between items-center w-full overflow-hidden relative">
+      {/* MOBILE NAV */}
+      <nav className="flex md:hidden w-full justify-between p-4">
         <a
-          className="border-2 p-2 ml-8 cursor-pointer"
+          className="border-2 p-2 cursor-pointer"
           href="https://annukul.vercel.app"
         >
-          <Image src="/images/initial.png" alt="Logo" width={40} height={10} />
+          <Image src="/images/initial.png" alt="Logo" width={20} height={10} />
         </a>
-        <div className={styles.navIcons}>
-          <a
-            href="https://annukul.hashnode.dev/"
-            className={styles.iconLink}
-            target="_blank"
-            rel="noreferrer"
-          >
+        <button onClick={() => setIsNavOpen(true)} className="outline-none">
+          <p className="text-white text-3xl">
+            <HiMenuAlt3 />
+          </p>
+        </button>
+      </nav>
+      {/* MOBILE NAV ENDS */}
+      {/* CLOSEABLE NAV */}
+      <div
+        style={{ backgroundColor: "#0A1930" }}
+        className={`${!isNavOpen && "-right-96"} md:hidden h-full w-3/5 absolute right-0 transition-all duration-200 ease-in-out`}
+      >
+        <div className="w-full h-16 flex justify-end px-4 py-4">
+          <button onClick={() => setIsNavOpen(false)} className="outline-none">
+            <p className="text-white text-3xl">
+              <AiOutlineClose />
+            </p>
+          </button>
+        </div>
+        <div className="text-slate-100 flex flex-col px-8 py-8">
+          <a href="#about-me" className={styles.iconLink}>
             About
           </a>
           <a
@@ -30,45 +50,71 @@ export const MainSection = () => {
           >
             Blog
           </a>
-          <a
-            href="https://www.linkedin.com/in/annukul-thakran/"
-            className={styles.iconLink}
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="#exp" className={styles.iconLink}>
             Experience
           </a>
           <a
-            href="https://github.com/Annukul"
+            href="#exp"
             className={styles.iconLink}
             target="_blank"
             rel="noreferrer"
           >
             Work
           </a>
+          <a href="#contact" className={styles.iconLink}>
+            Contact
+          </a>
+        </div>
+      </div>
+      {/* CLOSEABLE NAV ENDS */}
+      <nav className="w-full h-24 hidden md:flex justify-between items-center py-2 px-8">
+        <a
+          className="border-2 p-2 ml-8 cursor-pointer"
+          href="https://annukul.vercel.app"
+        >
+          <Image src="/images/initial.png" alt="Logo" width={40} height={10} />
+        </a>
+        <div className={styles.navIcons}>
+          <a href="#about-me" className={styles.iconLink}>
+            About
+          </a>
           <a
-            href="https://twitter.com/thakrananukul"
+            href="https://annukul.hashnode.dev/"
             className={styles.iconLink}
             target="_blank"
             rel="noreferrer"
           >
+            Blog
+          </a>
+          <a href="#exp" className={styles.iconLink}>
+            Experience
+          </a>
+          <a
+            href="#exp"
+            className={styles.iconLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Work
+          </a>
+          <a href="#contact" className={styles.iconLink}>
             Contact
           </a>
         </div>
       </nav>
       <div className="flex w-full h-full">
-        <div className="flex w-5/6 items-center justify-center">
-          <div className="flex flex-col justify-center pr-8 pl-32 text-left">
+        <div className="flex px-4 md:p-0 w-full md:w-5/6 items-center justify-center">
+          <div className="flex flex-col justify-center p-0 md:pr-8 md:pl-32 text-left">
             <div className="mb-4">
-              <p className="text-6xl font-medium text-white tracking-wide">
+              <p className="text-4xl md:text-6xl font-medium text-white tracking-wide">
                 Annukul Thakran.
               </p>
-              <p className="text-6xl font-medium text-slate-400 tracking-wide">
+              <p className="text-4xl md:text-6xl font-medium text-slate-400 tracking-wide">
                 I build things for the web.
               </p>
             </div>
             <div className="">
-              <p className="text-white text-xl">
+              <p className="text-white text-lg md:text-xl">
                 I am a versatile engineer with interests in software engineer
                 and designing. <br />
                 Adaptable and self motivated learner. I enjoy creating sleek and
@@ -81,7 +127,7 @@ export const MainSection = () => {
             </div>
           </div>
         </div>
-        <div className="flex h-full w-1/6 justify-end px-4">
+        <div className="hidden md:flex h-full w-1/6 justify-end px-4">
           <div className="flex flex-col mt-12 fixed">
             <a
               href="https://annukul.hashnode.dev/"
